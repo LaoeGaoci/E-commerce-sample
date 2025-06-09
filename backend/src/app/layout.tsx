3261450637f0a2da47ab0ee2edfe76eac5b42d96
lipeizhang99 <lipeizhang1@qq.com>
2025-06-09 13:50:11 +0800

    layout

--- backend/src/app/layout.tsx
+++ backend/src/app/layout.tsx
@@ -156,40 +156,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         },
       ],
     },
-    // {
-    //   separator: true,
-    // },
-    // {
-    //   command: () => {
-    //     toast.current.show({
-    //       severity: "info",
-    //       summary: "Info",
-    //       detail: "Item Selected",
-    //       life: 3000,
-    //     });
-    //   },
-    //   template: (item, options) => {
-    //     return (
-    //       <button
-    //         onClick={(e) => options.onClick(e)}
-    //         className={classNames(
-    //           options.className,
-    //           "w-full p-link flex align-items-center p-2 pl-4 text-color hover:surface-200 border-noround"
-    //         )}
-    //       >
-    //         <Avatar
-    //           image="https://primefaces.org/cdn/primereact/images/avatar/amyelsner.png"
-    //           className="mr-2"
-    //           shape="circle"
-    //         />
-    //         <div className="flex flex-column align">
-    //           <span className="font-bold">Amy Elsner</span>
-    //           <span className="text-sm">Agent</span>
-    //         </div>
-    //       </button>
-    //     );
-    //   },
-    // },
+    
   ];
 
   return (
